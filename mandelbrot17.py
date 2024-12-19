7179817597e2c584051c4d9dd6a590683d3dbe22
markus <markus@markus-HP-EliteBook-8470p>
2024-12-19 11:41:06 +0100

    Remove pbm file before the actual test.

--- mandelbrot17.py
+++ mandelbrot17.py
@@ -1,11 +1,16 @@
 from timeit import default_timer as timer
 from subprocess import call
 from sys import argv
+from os import remove
+from os import path
 
 name = "mandelbrot17"
 if len(argv) > 1:
   name = argv[1]
+fullPbmFileName = "{0}.pbm".format(name)
+if path.exists(fullPbmFileName):
+    remove(fullPbmFileName)
 start = timer()
-call("Release/{0} > {0}.pbm".format(name) , shell=True)
+call("Release/{0} > {1}".format(name, fullPbmFileName) , shell=True)
 end = timer()
 print(end - start)
